23b931f36703bdfe68ddfe76cbd65149def2d27c
Neely Arline <neelyarl@oa-j5.msk.21-school.ru>
2023-03-03 22:26:14 +0300

    v

--- src/vector/constructors.cc
+++ src/vector/constructors.cc
@@ -1,4 +1,26 @@
 #include "s21_vector.h"
 
-/* template <typename T, typename Allocator> */
-/* s21::vector<T, Allocator>::vector() : elem{nullptr}, space{nullptr}, last{nullptr} {} */
+using namespace s21;
+
+template <typename T, typename A>
+vector<T, A>::vector() : elem{nullptr},
+                         space{nullptr},
+                         last{nullptr},
+                         alloc{A()} {
+}
+
+template <typename T, typename A>
+vector<T, A>::vector(const A& a) noexcept : elem{nullptr}, space{nullptr}, last{nullptr}, alloc{a} {}
+
+template <typename T, typename A>
+vector<T,A>::vector(size_type count, const T& value, const A& a) : alloc{a} {
+  elem = alloc.allocate(count);
+  if (count)
+    try {
+      std::uninitialized_fill(elem, elem + count, value);
+      space = last = elem + count;
+    } catch (...) {
+      alloc.deallocate(elem, count);
+      throw;
+    }
+}
--- src/vector/s21_vector.h
+++ src/vector/s21_vector.h
@@ -7,49 +7,49 @@
 // think about what i will implement, i think idw to implement all of these constructors;
 // mb just do rule of 5, init_list and explicit one
 namespace s21 {
-  template<typename T, typename Allocator = std::allocator<T>>
+  template<typename T, typename A = std::allocator<T>>
   class vector {
     public:
       using size_type = unsigned int;
       using value_type = T;
       using reference = value_type&;
       // Default constructor. Constructs an empty container with a default-constructed allocator.
-      vector() = default;
+      vector();
 
       // Constructs an empty container with the given allocator alloc.
-      explicit vector(const Allocator& alloc) noexcept;
+      explicit vector(const A& alloc) noexcept;
 
       // Constructs the container with count copies of elements with value value.
       explicit vector(size_type count,
                       const T& value = T(),
-                      const Allocator& alloc = Allocator());
+                      const A& alloc = A());
 
       // Constructs the container with count default-inserted instances of T. No copies are made.
-      explicit vector(size_type count, const Allocator& alloc = Allocator());
+      explicit vector(size_type count, const A& alloc = A());
 
       // Constructs the container with the contents of the range [first, last). 
-      template<class InputIt> vector( InputIt first, InputIt last, const Allocator& alloc = Allocator() );
+      template<class InputIt> vector( InputIt first, InputIt last, const A& alloc = A() );
 
       // Copy constructor. Constructs the container with the copy of the contents of other.
       vector(const vector& other);
 
       // Constructs the container with the copy of the contents of other, using alloc as the allocator.
-      vector( const vector& other, const Allocator& alloc );
+      vector( const vector& other, const A& alloc );
       vector& operator=(const vector& other);
 
       // Move constructor. Constructs the container with the contents of other using move semantics.
-      // Allocator is obtained by move-construction from the allocator belonging to other.
+      // A is obtained by move-construction from the allocator belonging to other.
       // After the move, other is guaranteed to be empty().
       vector(vector&& other) noexcept;
 
-      // Allocator-extended move constructor. Using alloc as the allocator for the new container,
+      // A-extended move constructor. Using alloc as the allocator for the new container,
       // moving the contents from other; if alloc != other.get_allocator(), this results in an element-wise move.
       // (In that case, other is not guaranteed to be empty after the move.)
-      vector( vector&& other, const Allocator& alloc );
+      vector( vector&& other, const A& alloc );
       vector& operator=(vector&& other) noexcept;
 
       // Constructs the container with the contents of the initializer list init.
-      vector(std::initializer_list<T> init, const Allocator& alloc = Allocator());
+      vector(std::initializer_list<T> init, const A& alloc = A());
 
       ~vector();
 
@@ -59,7 +59,7 @@ namespace s21 {
       T* elem; // начало выделяемой памяти
       T* space; // начало области памяти для расширения, конец последовательности элементов
       T* last; // конец выделенной памяти
-      Allocator alloc; // аллокаторб для запрашивания памяти под элементы
+      A alloc; // аллокаторб для запрашивания памяти под элементы
   };
 } // namespace s21
 
